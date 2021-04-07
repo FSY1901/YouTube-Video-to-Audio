8adf99d1ffce3ff1a665dabd810cbf01c7497497
AMS <bektasevicamar1901@gmail.com>
2021-04-07 19:10:08 +0200

    Added ReadMe

---
+++ Converter/main.py
@@ -0,0 +1,13 @@
+import pafy
+
+while True:
+    url = input("Enter the url: \n")
+    video = pafy.new(url)
+    audio = video.audiostreams
+    # D:\AmarB\Music
+    folder = input("Enter location: \n")
+
+    for a in audio:
+        print('bitrate : %s, ext: %s, size: %0.2fMb' % (a.bitrate, a.extension, a.get_filesize() / 1024 / 1024))
+
+    audio[3].download(filepath=folder)
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # YouTube Video to Audio
  This script takes the audio data of a video and places it inside the given location.
+
+Use this to doenload music for yourself, but don't ever re-publish or use the music for commercial uses!
